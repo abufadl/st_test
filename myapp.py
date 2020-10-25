d391257a06b5665a0981623014a6f5d2569aff02
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-25 20:44:21 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -24,3 +24,12 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+#widgets
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+    
