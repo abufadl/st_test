8baf19ab39f0ef966955973e66c49e20a7e85322
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-25 20:39:25 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -10,3 +10,10 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
+
+# chart
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
