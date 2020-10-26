478eb4bdebc83a05dbc3ccbe04ae9e2338bc88ab
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-26 20:15:18 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import time
+
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
 import numpy as np
@@ -69,3 +71,18 @@ else:
 
 expander = st.beta_expander("FAQ")
 expander.write("Here you could put in some really, really long explanations...")
+
+# sleep
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
