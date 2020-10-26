dda3ac3e59457aea1e7685421d78ead9327ad622
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-26 20:09:59 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -62,7 +62,8 @@ st.sidebar.markdown("""
 left_column, right_column = st.beta_columns(2)
 pressed = left_column.button('Press me?')
 if pressed:
-    right_column.write("Woohoo!")
+    right_column.write("You pressed my neighbor!")
+    pressed = False
 
 expander = st.beta_expander("FAQ")
 expander.write("Here you could put in some really, really long explanations...")
