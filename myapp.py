20d3dabeb1f3b05d294456068b2afcbfc4d0dad7
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-26 20:07:24 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -56,4 +56,13 @@ st.sidebar.markdown("""
 ## My great app
 """
 )
-st.sidebar.slider(0,10)
+#st.sidebar.slider(0,10) # error sytax?
+
+#columns
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('Press me?')
+if pressed:
+    right_column.write("Woohoo!")
+
+expander = st.beta_expander("FAQ")
+expander.write("Here you could put in some really, really long explanations...")
