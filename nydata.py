65653e2dac1291d91cfafee2cba03647503d52d0
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-27 20:44:29 +0200

    Update nydata.py

--- nydata.py
+++ nydata.py
@@ -5,7 +5,7 @@ import numpy as np
 # title
 st.title('Uber pickups in NYC')
 
-st.write(st.version)
+st.write(f" Streamlit version:{st.__version__}")
 # get data
 DATE_COLUMN = 'date/time'
 DATA_URL = 'https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz'
