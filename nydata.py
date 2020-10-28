dd5f5cb0298972784278d0bb58971b5b90fe399b
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-28 19:58:39 +0200

    Update nydata.py

--- nydata.py
+++ nydata.py
@@ -131,3 +131,13 @@ status_text.text('Done!')
 st.balloons()
 
 
+# widgets 
+genre = st.radio(
+    "What's your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn't select comedy.")
+
