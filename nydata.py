ae6558c2a0768959413af4a8e977feb89d272bfc
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-28 19:31:24 +0200

    Update nydata.py

--- nydata.py
+++ nydata.py
@@ -100,3 +100,29 @@ my_slot1.text('This will appear second')
 
 my_slot2.line_chart(np.random.randn(20, 2))
 # Replaces the second empty slot with a chart.
+
+# animations
+progress_bar = st.progress(0)
+status_text = st.empty()
+chart = st.line_chart(np.random.randn(10, 3))
+
+for i in range(100):
+    # Update progress bar.
+    progress_bar.progress(i + 1)
+
+    new_rows = np.random.randn(10, 3)
+
+    # Update status text.
+    status_text.text(
+        'The latest random number is: %s' % new_rows[-1, 1])
+
+    # Append data to the chart.
+    chart.add_rows(new_rows)
+
+    # Pretend we're doing some computation that takes time.
+    time.sleep(0.1)
+
+status_text.text('Done!')
+st.balloons()
+
+
