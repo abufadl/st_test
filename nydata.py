aab620c93c5ba743d286812465d37b992135a326
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-28 18:53:03 +0200

    Update nydata.py

--- nydata.py
+++ nydata.py
@@ -76,5 +76,27 @@ with right_column:
     chosen = st.radio(
         'Sorting hat',
         ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    #st.write(f"You are in {chosen} house!") # show in sidebar
     
 st.write(f"You are in {chosen} house!")
+
+
+# slots 
+st.text('This will appear first')
+# Appends some text to the app.
+
+my_slot1 = st.empty()
+# Appends an empty slot to the app. We'll use this later.
+
+my_slot2 = st.empty()
+# Appends another empty slot.
+
+st.text('This will appear last')
+# Appends some more text to the app.
+
+# do sufff and go back  to empty slots 
+my_slot1.text('This will appear second')
+# Replaces the first empty slot with a text string.
+
+my_slot2.line_chart(np.random.randn(20, 2))
+# Replaces the second empty slot with a chart.
