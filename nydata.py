6bdca59865b48c77dc7aa4ee640953822c69d9ad
abufadl <61349083+abufadl@users.noreply.github.com>
2020-10-28 20:08:59 +0200

    Update nydata.py

--- nydata.py
+++ nydata.py
@@ -141,5 +141,17 @@ if genre == 'Comedy':
 else:
     st.write("You didn't select comedy.")
 
-mytext = st.text_input('review', '', max_chars=25)
+mytext = st.text_input('review', '', max_chars=250)
 st.write('The review is', mytext)
+
+# text area
+def run_sentiment_analysis(t):
+    return 'Positive with 0.923 confidence'
+txt = st.text_area('Text to analyze', '''
+     It was the best of times, it was the worst of times, it was
+     the age of wisdom, it was the age of foolishness, it was
+     the epoch of belief, it was the epoch of incredulity, it
+     was the season of Light, it was the season of Darkness, it
+     was the spring of hope, it was the winter of despair, (...)
+     ''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
